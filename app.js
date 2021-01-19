dd5365babc19d43645623f00df68ac215e338c6b
Henry Chung <chungjenhao81@gmail.com>
2021-01-19 09:33:00 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // require package used in the project
 const express = require("express");
 const exphbs = require("express-handlebars");
+const hbsHelpers = require('./helpers/handlebars')
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 
@@ -11,7 +12,7 @@ const app = express();
 const port = 3000;
 
 // setting template engine
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs", helpers: hbsHelpers }));
 app.set("view engine", "hbs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,12 +7,22 @@ const RestaurantListModels = require("../../models/restaurant")
 
 // 定義首頁路由
 router.get("/", (req, res) => {
-  // past the restaurant data into 'index' partial template
-  RestaurantListModels.find() // 取出 RestaurantListModels 裡面的所有資料
+  const keyword = req.query.keyword || ''
+  const sort = req.query.sort || ''
+  RestaurantListModels.find(
+    {
+      $or: [
+        { name: { $regex: keyword, $options: 'i' } },
+        { category: { $regex: keyword, $options: 'i' } }
+      ]
+    })
+    // past the restaurant data into 'index' partial template
     .lean() //把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(RestaurantListModels => res.render("index", { RestaurantListModels })) // 將資料傳給 index 樣板
+    .sort({ [sort]: 'asc' })
+    .then(RestaurantListModels =>
+      res.render("index", { RestaurantListModels, keyword, sort })) // 將資料傳給 index 樣板
     .catch(error => console.log(error)) //錯誤處理
-});
+})
 
 // 匯出路由模組
 module.exports = router
