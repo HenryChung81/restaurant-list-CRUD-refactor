5ee2f70a2e75aeaec794282fee0c59e640f649ed
Henry Chung <chungjenhao81@gmail.com>
2021-05-22 20:15:23 +0800

    fix: image input

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 // require package used in the project
-const express = require("express");
-const exphbs = require("express-handlebars");
-const hbsHelpers = require('./helpers/handlebars')
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
-const routes = require("./routes")
+const routes = require('./routes')
 require('./config/mongoose')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 // setting template engine
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs", helpers: hbsHelpers }));
-app.set("view engine", "hbs");
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // setting static files
-app.use(express.static("public"));
+app.use(express.static('public'))
 
 app.use(methodOverride('_method'))
 
@@ -26,5 +25,5 @@ app.use(routes)
 
 // start and listen the Express server
 app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`);
-});
+  console.log(`Express is listening on http://localhost:${port}`)
+})
